59c68119b200f032f7cf342c5b05fa9f9d7ae6f7
Lucas Marcolongo <lucas_marco@live.com>
2024-08-03 20:20:59 -0700

    fix(helm): fix chart generator tests

--- packages/helm/src/generators/chart/generator.spec.ts
+++ packages/helm/src/generators/chart/generator.spec.ts
@@ -5,20 +5,21 @@ import {
 } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
 
-import { DEFAULT_OPTIONS } from './constants';
 import { chartGenerator } from './generator';
 import { ChartGeneratorSchema } from './schema.d';
 
 describe('chart generator', () => {
   let tree: Tree;
-  const options: ChartGeneratorSchema = {
-    ...DEFAULT_OPTIONS,
-    name: 'test-chart',
-    project: 'test-project',
-  };
+  let options: ChartGeneratorSchema;
 
   beforeAll(async () => {
     tree = createTreeWithEmptyWorkspace();
+
+    options = {
+      name: 'test-chart',
+      project: 'test-project',
+      chartFolder: 'chart',
+    };
 
     addProjectConfiguration(tree, options.project, {
       root: `'${options.project}'`,
@@ -37,8 +38,29 @@ describe('chart generator', () => {
     expect(tree.exists(`${projectConfig.root}/chart/Chart.yaml`)).toBe(true);
   });
 
+  it('should create the chart in a custom folder', async () => {
+    options = { ...options, chartFolder: 'custom-folder' };
+    await chartGenerator(tree, options);
+    const projectConfig = readProjectConfiguration(tree, options.project);
+    expect(tree.exists(`${projectConfig.root}/custom-folder/Chart.yaml`)).toBe(
+      true,
+    );
+  });
+
   it('should update the project configuration', async () => {
     const projectConfig = readProjectConfiguration(tree, options.project);
     expect(projectConfig.targets['helm']).toBeDefined();
+    expect(projectConfig.targets['helm'].executor).toBe(
+      '@nx-extensions/helm:package',
+    );
+    expect(projectConfig.targets['helm'].outputs).toEqual([
+      '{options.outputFolder}',
+    ]);
+    expect(projectConfig.targets['helm'].options).toEqual({
+      chartFolder: `${projectConfig.root}/${options.chartFolder}`,
+      outputFolder: '{workspaceRoot}/charts/{projectRoot}',
+      push: false,
+      remote: 'oci://localhost:5000/helm-charts',
+    });
   });
 });
