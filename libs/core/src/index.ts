4e423920715c7e77b9fcf663ffe847a2b7a9d3cb
Lucas Marcolongo <lucas_marco@live.com>
2024-08-06 21:32:27 -0700

    feat(core): add `addToPrettierIgnore` util function

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/utils/helm';
+export * from './lib/utils/prettier';
---
+++ libs/core/src/lib/utils/prettier.ts
@@ -0,0 +1,64 @@
+import { Tree } from '@nx/devkit';
+
+import { PrettierConfig } from './types';
+
+/**
+ * Resolves the existing Prettier configuration.
+ *
+ * @returns {Promise<ExistingPrettierConfig | undefined>}
+ */
+export async function resolveUserExistingPrettierConfig(): Promise<
+  PrettierConfig | undefined
+> {
+  let prettier: typeof import('prettier');
+  try {
+    // eslint-disable-next-line unicorn/prefer-module
+    prettier = require('prettier');
+  } catch {
+    return undefined;
+  }
+
+  if (!prettier) {
+    return undefined;
+  }
+  try {
+    const filepath = await prettier.resolveConfigFile();
+    if (!filepath) {
+      return undefined;
+    }
+
+    const config = await prettier.resolveConfig(process.cwd(), {
+      useCache: false,
+      config: filepath,
+    });
+    if (!config) {
+      return undefined;
+    }
+
+    return {
+      sourceFilepath: filepath,
+      config: config,
+    };
+  } catch {
+    return undefined;
+  }
+}
+
+export function addToPrettierIgnore(tree: Tree, ignore: string[]) {
+  const ignorePath = `${tree.root}/.prettierignore`;
+
+  if (!tree.exists(ignorePath)) {
+    tree.write(ignorePath, ignore.join('\n'));
+    return;
+  }
+
+  try {
+    const content = tree.read(ignorePath, 'utf8') || '';
+    const lines = content.split('\n');
+    // Remove duplicates
+    const newContent = [...new Set([...lines, ...ignore])].join('\n');
+    tree.write(ignorePath, newContent);
+  } catch (error: unknown) {
+    throw new Error(`Failed to update .prettierignore file: ${String(error)}`);
+  }
+}
